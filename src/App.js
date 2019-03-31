69a299fb978c5d6b983aa5d79748af542e06fe73
Matt Seto <seanseto@gmail.com>
2019-03-31 00:44:10 -0700

    Move cdn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Layout from './components/Numenera/Layout';
-// Materialize CSS
-// import '../node_modules/materialize-css/dist/css/materialize.min.css';
-// import '../node_modules/materialize-css/dist/js/materialize.min.js';
 import './app.scss';
 
 export default function App() {
