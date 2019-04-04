044172051b7a3d3e9308bef1f73d9873426b2d18
Matt Seto <seanseto@gmail.com>
2019-04-03 17:53:17 -0700

    PWA stuff.

---
+++ buildref/precache-manifest.6aa83dabfd5e849446275abfb6deda26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d214f8526fcfe1956985",
+    "url": "./static/js/runtime~main.5484cc5d.js"
+  },
+  {
+    "revision": "b8dc9a122b5bbbe5506f",
+    "url": "./static/js/main.8e60ded4.chunk.js"
+  },
+  {
+    "revision": "3e86d9f706a9481518e5",
+    "url": "./static/js/2.166b2a14.chunk.js"
+  },
+  {
+    "revision": "b8dc9a122b5bbbe5506f",
+    "url": "./static/css/main.f313f3d3.chunk.css"
+  },
+  {
+    "revision": "3e86d9f706a9481518e5",
+    "url": "./static/css/2.8b068af5.chunk.css"
+  },
+  {
+    "revision": "a10c233ea5b377b74a4948232ff948cf",
+    "url": "./index.html"
+  }
+];
